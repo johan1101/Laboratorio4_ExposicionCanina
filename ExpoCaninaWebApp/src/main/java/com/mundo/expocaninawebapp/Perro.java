768514371958f939b0bacccecbfdf3bcf9d7832a
AnaCristina00 <124208761+AnaCristina00@users.noreply.github.com>
2023-09-21 18:53:57 -0500

    Agregando la clase Perro para la creación de objetos de tipo Perro

---
+++ ExpoCaninaWebApp/src/main/java/com/mundo/expocaninawebapp/Perro.java
@@ -0,0 +1,62 @@
+package com.mundo.expocaninawebapp;
+
+public class Perro {
+    
+    private String nombre;
+    private String raza;
+    private String imagen;
+    private int puntos;
+    private int edad;
+
+    public Perro() {
+    }
+
+    public Perro(String nombre, String raza, String imagen, int puntos, int edad) {
+        this.nombre = nombre;
+        this.raza = raza;
+        this.imagen = imagen;
+        this.puntos = puntos;
+        this.edad = edad;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getRaza() {
+        return raza;
+    }
+
+    public void setRaza(String raza) {
+        this.raza = raza;
+    }
+
+    public String getImagen() {
+        return imagen;
+    }
+
+    public void setImagen(String imagen) {
+        this.imagen = imagen;
+    }
+
+    public int getPuntos() {
+        return puntos;
+    }
+
+    public void setPuntos(int puntos) {
+        this.puntos = puntos;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+    
+}
